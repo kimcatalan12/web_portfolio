76a23e040b281fece41627fb9e518b90b433021c
kimcatalan12@gmail.com <kimcatalan12.com>
2024-09-20 00:10:49 +0800

    navbar scrollpsy done

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-const date = new Date().getFullYear();
+document.querySelectorAll('#nav-tab>[data-bs-toggle="tab"]').forEach(el => {
+    el.addEventListener('shown.bs.tab', () => {
+      const target = el.getAttribute('data-bs-target')
+      const scrollElem = document.querySelector(`${target} [data-bs-spy="scroll"]`)
+      bootstrap.ScrollSpy.getOrCreateInstance(scrollElem).refresh()
+    })
+})
 
-$("#date").text(date + " KIM CATALAN. ALL RIGHTS RESERVED.");
+$(document).ready(function () {
+    
+    const date = new Date().getFullYear();
+
+    $("#date").text(date + " KIM CATALAN. ALL RIGHTS RESERVED.");
+    
+    $(window).on('scroll', function () {
+        var scrollPos = $(document).scrollTop();
+        var sections = ['#scrollspyHeading1', '#scrollspyHeading2', '#scrollspyHeading3', '#scrollspyHeading4', '#scrollspyHeading5'];
+        var navbarLinks = $('#navbarheading a');
+        
+        sections.forEach(function(section, index) {
+          var currSection = $(section);
+          var sectionOffset = currSection.offset().top;
+          
+          if (scrollPos >= sectionOffset - 50 && scrollPos < sectionOffset + currSection.outerHeight()) {
+            navbarLinks.removeClass('active');
+            navbarLinks.eq(index).addClass('active');
+          }
+        });
+      });
+});
+
