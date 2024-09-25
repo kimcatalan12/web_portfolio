fe315cbdc6538e938e6d764e0ea5ec80dd71de40
Kim Catalan <kimcatalan12@gmail.com>
2024-09-25 17:13:08 +0800

    adding content and perfect alignment

--- index.js
+++ index.js
@@ -8,24 +8,38 @@ document.querySelectorAll('#nav-tab>[data-bs-toggle="tab"]').forEach(el => {
 
 $(document).ready(function () {
     
-    const date = new Date().getFullYear();
+  const date = new Date().getFullYear();
 
-    $("#date").text(date + " KIM CATALAN. ALL RIGHTS RESERVED.");
+  $("#date").text(date + " KIM CATALAN. ALL RIGHTS RESERVED.");
+  
+  $(window).on('scroll', function () {
+    var windowWidth = $(window).width();
+    var scrollPos = $(document).scrollTop();
     
-    $(window).on('scroll', function () {
-        var scrollPos = $(document).scrollTop();
-        var sections = ['#scrollspyHeading1', '#scrollspyHeading2', '#scrollspyHeading3', '#scrollspyHeading4', '#scrollspyHeading5'];
-        var navbarLinks = $('#navbarheading a');
+    // Only run the scrollspy logic if the window is wider than 768px
+    if (windowWidth > 768) {
+      var sections = ['#scrollspyHeading1', '#scrollspyHeading2', '#scrollspyHeading3', '#scrollspyHeading4', '#scrollspyHeading5'];
+      var navbarLinks = $('#navbarheading a');
+      
+      sections.forEach(function(section, index) {
+        var currSection = $(section);
+        var sectionOffset = currSection.offset().top;
         
-        sections.forEach(function(section, index) {
-          var currSection = $(section);
-          var sectionOffset = currSection.offset().top;
-          
-          if (scrollPos >= sectionOffset - 50 && scrollPos < sectionOffset + currSection.outerHeight()) {
-            navbarLinks.removeClass('active');
-            navbarLinks.eq(index).addClass('active');
-          }
-        });
+        if (scrollPos >= sectionOffset - 50 && scrollPos < sectionOffset + currSection.outerHeight()) {
+          navbarLinks.removeClass('active');
+          navbarLinks.eq(index).addClass('active');
+        }
       });
+    } else {
+      // Optional: Remove active class when the media query is met
+      $('#navbarheading a').removeClass('active');
+    }
+  });
+
+  // Ensure the script runs on window resize as well to adapt to media query changes
+  $(window).on('resize', function() {
+    $(window).trigger('scroll');
+  });
+
 });
 
